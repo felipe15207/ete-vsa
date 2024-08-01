abfb0be00040d0bf4a7be45d1d786d95a8c6aece
Lucas Kauã <168223952+LucasKaua17@users.noreply.github.com>
2024-08-01 15:46:40 -0300

    Atualizando

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+/* FUNÇÃO RESPONSÁVEL POR ABRIR O MENU */
+
+$(document).ready(function(){
+    $('#btn-menu').click(function(){
+        $('#btn-menu').toggleClass('ativado');
+        $('.lista-menu').toggleClass('ativado');
+    });
+  });
+
+/* FUNÇÃO RESPONSÁVEL POR ABRIR O MENU */
